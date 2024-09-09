9cf65eea0b29d7f309512f78a46d01287508f66f
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-09 15:14:55 +0200

    exo fini

--- 11-Les_strings/11_3-Travailler_avec_strings/P11_3_a-Verite_et_operations/src/main/kotlin/main.kt
+++ 11-Les_strings/11_3-Travailler_avec_strings/P11_3_a-Verite_et_operations/src/main/kotlin/main.kt
@@ -1,5 +1,10 @@
 fun main() {
-    // write your code here
-
+    val a = "sum"
+    val b= "mer"
+    val c = "summer"
+    if (a.length +b.length == c.length){
+        println("True")
+    }  else {
+        println("False")
+    }
 }
-
