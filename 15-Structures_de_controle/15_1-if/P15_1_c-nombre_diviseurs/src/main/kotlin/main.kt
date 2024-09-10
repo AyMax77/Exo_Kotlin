5c8ff3b385fa809de8c20c1ef6bacbfa55f922d8
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-10 08:31:57 +0200

    exo fini

--- 15-Structures_de_controle/15_1-if/P15_1_c-nombre_diviseurs/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_1-if/P15_1_c-nombre_diviseurs/src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    // put your code here
+    val a = 6
+    if(a % 2 == 0){
+        println("divisible par 2")
+    }
+    if(a%3==0){
+        println("divisible par 3")
+    }
+    if(a%6==0){
+        println("divisible par 6")
+    }
 }
