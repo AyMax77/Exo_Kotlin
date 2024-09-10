0472cdada9e04ff780b180cc1db45beec6cd0489
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-10 10:45:43 +0200

    exo fini

--- 16-Listes_mutables/16_1-Introduction/P16_1_b-Init_list/src/main/kotlin/main.kt
+++ 16-Listes_mutables/16_1-Introduction/P16_1_b-Init_list/src/main/kotlin/main.kt
@@ -1,6 +1,8 @@
 fun main() {
-    val numbers = // put your code here
-
-        // do not touch the lines below
-        println(numbers.joinToString())
+    val numbers = MutableList(100) { 0 }
+    numbers[0] = 1
+    numbers[9] = 10
+    numbers[99] = 100
+    println(numbers.joinToString())
 }
+
