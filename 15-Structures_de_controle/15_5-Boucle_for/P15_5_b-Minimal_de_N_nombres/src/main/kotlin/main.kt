ae8caa90be740089bd0eda87d0b3b0e99c8645f5
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-10 09:35:23 +0200

    exo fini

--- 15-Structures_de_controle/15_5-Boucle_for/P15_5_b-Minimal_de_N_nombres/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_5-Boucle_for/P15_5_b-Minimal_de_N_nombres/src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    // write your code here
+    var a = mutableListOf(5,5,1,4,2,3)
+    var res = a[0]
+    for (i in 1..< a.size){
+        if (a[i]< res){
+            res = a[i]
+        }
+    }
+    println(res)
 }
