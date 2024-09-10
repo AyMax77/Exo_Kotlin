da9511ac4f7b46770e51fd499984a31f6dbc8fb0
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-10 11:54:18 +0200

    exo fini*

--- 16-Listes_mutables/16_4-Boucles_for_sur_listes/P16_4_c-Triples/src/main/kotlin/main.kt
+++ 16-Listes_mutables/16_4-Boucles_for_sur_listes/P16_4_c-Triples/src/main/kotlin/main.kt
@@ -1,4 +1,18 @@
 fun main() {
-    // write your code here
-}
+    // Lire la taille de la liste
+    val size = readLine()!!.toInt()
 
+    // Lire les éléments de la liste
+    val numbers = mutableListOf(6,1)
+
+    // Variable pour compter le nombre de triples
+    var tripleCount = 0
+
+    // Vérifier les triples dans la liste
+    for (i in 0 until size - 2) {
+        if (numbers[i] + 1 == numbers[i + 1] && numbers[i + 1] + 1 == numbers[i + 2]) {
+            tripleCount++
+        }
+    }
+    println(tripleCount)
+}
