4c64bf7aaa49de374786f09277bf219b7f15639a
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-17 08:42:27 +0200

    Exo fini

--- 17-Programmation_procedurale/17_1-Appel_fonction/P17_1_a-Pains/src/main/kotlin/main.kt
+++ 17-Programmation_procedurale/17_1-Appel_fonction/P17_1_a-Pains/src/main/kotlin/main.kt
@@ -7,7 +7,6 @@ fun totalLembas(first: String, second: String) {
 fun main() {
     val breadFromFrodo = readln()
     val breadFromSam = readln()
-
-    // write your code here
+    totalLembas(breadFromFrodo,breadFromSam)
 
 }
