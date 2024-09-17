ab5e34de1939d857b94e4cf111f57a732742d1f1
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-17 09:04:45 +0200

    Exo fini

--- 17-Programmation_procedurale/17_2-Declaration_fonction/P17_2_c-Voyelle/src/main/kotlin/main.kt
+++ 17-Programmation_procedurale/17_2-Declaration_fonction/P17_2_c-Voyelle/src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun isVowel(lettre:Char){
-    println(lettre =='a' || lettre == 'e' || lettre == 'i' || lettre == 'o' || lettre=='u' || lettre=='y')
+    println(lettre =='a' || lettre == 'e' || lettre == 'i' || lettre == 'o' || lettre=='u' || lettre=='y'||lettre=='A'||lettre=='E'||lettre=='I'||lettre=='O'||lettre=='U'||lettre=='Y')
 }
 
 fun main() {
