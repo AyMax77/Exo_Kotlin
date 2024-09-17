815583598ec65c0044b47c56d5d307babacc95a1
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-17 10:45:20 +0200

    Exo fini

--- 17-Programmation_procedurale/17_3-Arguments_defaut/P17_3_b-Gratuit/src/main/kotlin/main.kt
+++ 17-Programmation_procedurale/17_3-Arguments_defaut/P17_3_b-Gratuit/src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
-fun tip(bill: Int, percentage: Int): Int {
-    // TODO
-}
+fun tip(bill: Int, percentage: Int=10): Int {
+    return(bill*percentage/100)
+    }
 
 fun main() {
     println(tip(100))
-    println(tip(100, 5))
+    println(tip(50))
 }
