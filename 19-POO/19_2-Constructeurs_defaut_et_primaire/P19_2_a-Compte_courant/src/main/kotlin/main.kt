c1011f33e9f25c6268ba3c922d0436ec4abc74c8
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-23 17:14:32 +0200

    Exo fini

--- 19-POO/19_2-Constructeurs_defaut_et_primaire/P19_2_a-Compte_courant/src/main/kotlin/main.kt
+++ 19-POO/19_2-Constructeurs_defaut_et_primaire/P19_2_a-Compte_courant/src/main/kotlin/main.kt
@@ -1,7 +1,13 @@
-// write the BankAccount class here
+class BankAccount(deposite:Int,withdrawn:Int){
+    var deposite = 0
+    var withdrawn = 0
+    var balance = deposite - withdrawn
+}
 
 
 fun main() {
     val account = BankAccount(100, 20)
+    println(account.deposite)
+    println(account.withdrawn)
     println(account.balance)
 }
