51656b3c40f9690f44c186a66c9d268f3e2f3ed2
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-24 09:34:41 +0200

    Exo fini

--- 19-POO/19_3-Methodes/P19_3_c-Automobile/src/main/kotlin/main.kt
+++ 19-POO/19_3-Methodes/P19_3_c-Automobile/src/main/kotlin/main.kt
@@ -1,12 +1,21 @@
-class Car(val make: String, val year: Int) {
+class Car(val make: String, val year: Int,speed:Int) {
 
     var speed: Int = 0
+    fun accelerate(){
+        speed+=5
+    }
+    fun decelerate(){
+        speed-=5
+        if(speed<0){
+            speed = 0
+        }
+    }
 
-    // write the methods here
+
 }
 
 fun main() {
-    val myCar = Car("Mustang", 1976)
+    val myCar = Car("Mustang", 1976,100)
     myCar.accelerate()
     println(myCar.speed)
     myCar.decelerate()
