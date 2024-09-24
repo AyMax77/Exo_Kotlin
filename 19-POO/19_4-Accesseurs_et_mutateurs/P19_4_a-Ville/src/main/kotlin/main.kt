726897489aaa9a68f5b1352e2965d7d811a50b75
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-24 11:06:50 +0200

    Exo fini

--- 19-POO/19_4-Accesseurs_et_mutateurs/P19_4_a-Ville/src/main/kotlin/main.kt
+++ 19-POO/19_4-Accesseurs_et_mutateurs/P19_4_a-Ville/src/main/kotlin/main.kt
@@ -1,14 +1,17 @@
-class City(val name: String) {
-    var population: Int
+class City(val name: String,var population:Int) {
+    init {
+        if (population < 0) {
+            population = 0
+        } else if(population>50_000_000){
+            population = 50_000_000
+        }
+    }
 }
 
 fun main() {
-    val goodCity = City("Good City")
-    val badCity = City("Bad City")
+    val goodCity = City("Good City",65_000_000)
+    val badCity = City("Bad City",45_000_000)
 
-    badCity.population = 65_000_000
     println(badCity.population)
-
-    goodCity.population = 45_000_000
     println(goodCity.population)
 }
