f96600155ac6cf0c333722cdde80c9d15426e1c0
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-09-24 11:40:23 +0200

    Exo fini

--- 19-POO/19_5-Constructeurs_secondaires/P19_5_a-Kitty/src/main/kotlin/main.kt
+++ 19-POO/19_5-Constructeurs_secondaires/P19_5_a-Kitty/src/main/kotlin/main.kt
@@ -1,5 +1,20 @@
 class Kitty {
-    // write here
+    var color:String=""
+    var age:Int=0
+    constructor(_color: String,_age:Int){
+        var color = _color
+        var age = _age
+    }
+    constructor(__age:Int,__color:String){
+        var age = __age
+        var color = __color
+    }
+    constructor(couleur:String){
+        var color = couleur
+    }
+    constructor(ager:Int){
+        var age = ager
+    }
 }
 
 
